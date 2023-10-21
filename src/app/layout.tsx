bd1e3feb163c8fbd82e9afec9a50e35a8b5fde42
Ayush Kumar <ayush2000mickey@gmail.com>
2023-10-21 14:21:11 +0530

    feat : ms clarity and GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
+// import { useEffect } from "react";
+// import { useRouter } from "next/navigation";
+// import * as ga from "../utils/analytics";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +18,52 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  // const router = useRouter();
+
+  // useEffect(() => {
+  //   const unsubscribe = router.subscribe(
+  //     "routeChangeComplete",
+  //     ({ url }: any) => {
+  //       ga.pageview(url);
+  //     }
+  //   );
+  //   return () => {
+  //     unsubscribe();
+  //   };
+  // }, [router]);
+
   return (
     <html lang="en">
+      <head>
+        {/* Microsoft Clarity */}
+        <script>
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, 'clarity', 'script', ${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID});
+          `}
+        </script>
+
+        {/* Google Analytics */}
+        {/* {ga.GA_TRACKING_ID && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${ga.GA_TRACKING_ID}`}
+            ></script>
+            <script>
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${ga.GA_TRACKING_ID}');
+              `}
+            </script>
+          </>
+        )} */}
+      </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
---
+++ src/utils/analytics.ts
@@ -0,0 +1,13 @@
+// export const GA_TRACKING_ID = "UA-XXXXXXXXX-X"; // Replace with your GA tracking ID
+
+// // Log the pageview with their URL
+// export const pageview = (url: string) => {
+//   window.gtag("config", GA_TRACKING_ID, {
+//     page_path: url,
+//   });
+// };
+
+// // Log specific events:
+// export const event = ({ action, params }: { action: string; params: any }) => {
+//   window.gtag("event", action, params);
+// };
