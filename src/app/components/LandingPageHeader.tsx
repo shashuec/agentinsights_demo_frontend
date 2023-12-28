84539743b55d79e551e3ea419acd691ee035031f
Ayush Sachan <sachan.ayush47@gmail.com>
2023-12-28 12:15:51 +0530

    Update LandingPageHeader.tsx

--- src/app/components/LandingPageHeader.tsx
+++ src/app/components/LandingPageHeader.tsx
@@ -141,7 +141,7 @@ const LandingPageHeader = () => {
                 SignIn / Login
               </Box>
             </Link>
-            <Link href="/demo?uuid=mH1kAN4rNDciWTYBWWh5">
+            <Link href="/demo?uuid=9f04faa8-2796-4394-afa9-b2e8a714b317">
               <Box className="p-2 font-semibold bg-blue-500 text-white text-center rounded transition duration-300 ease-in-out">
                 Try Demo
               </Box>
